2410497fbd71e0d29648446a3fa5ec9d0b67c240
Phantomlancer123 <fronzen615heart@gmail.com>
2024-06-26 14:15:46 +0200

    fix: remove spaces to fit coding style

--- 2. NodeJS Stock Trades API/project/controllers/trades.js
+++ 2. NodeJS Stock Trades API/project/controllers/trades.js
@@ -16,40 +16,40 @@ exports.createTrade = async (req, res) => {
 };
 
 exports.getTrades = async (req, res) => {
-    try {
-      const { type, user_id } = req.query;
-      const where = {};
-  
-      if (type) {
-        where.type = type;
-      }
-  
-      if (user_id) {
-        where.user_id = user_id;
-      }
-  
-      const trades = await Trade.findAll({ where, order: [['id', 'ASC']] });
-      res.status(200).json(trades);
-    } catch (error) {
-      res.status(500).send();
+  try {
+    const { type, user_id } = req.query;
+    const where = {};
+
+    if (type) {
+      where.type = type;
     }
+
+    if (user_id) {
+      where.user_id = user_id;
+    }
+
+    const trades = await Trade.findAll({ where, order: [['id', 'ASC']] });
+    res.status(200).json(trades);
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 exports.getTradeById = async (req, res) => {
-    try {
-      const trade = await Trade.findByPk(req.params.id);
-  
-      if (!trade) {
-        return res.status(404).send('ID not found');
-      }
-  
-      res.status(200).json(trade);
-    } catch (error) {
-      res.status(500).send();
+  try {
+    const trade = await Trade.findByPk(req.params.id);
+
+    if (!trade) {
+      return res.status(404).send('ID not found');
     }
+
+    res.status(200).json(trade);
+  } catch (error) {
+    res.status(500).send();
+  }
 };
 
 exports.methodNotAllowed = (req, res) => {
-    res.status(405).send();
+  res.status(405).send();
 };
   
--- 2. NodeJS Stock Trades API/project/models/trades.js
+++ 2. NodeJS Stock Trades API/project/models/trades.js
@@ -9,42 +9,42 @@ const sequelize = new Sequelize("sqlite::memory:");
 // Insert your model definition below
 
 const Trade = sequelize.define('Trade', {
-    id: {
-      type: DataTypes.INTEGER,
-      autoIncrement: true,
-      primaryKey: true,
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  type: {
+    type: DataTypes.ENUM,
+    values: ['buy', 'sell'],
+    allowNull: false,
+  },
+  user_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  symbol: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  shares: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      min: 1,
+      max: 100,
     },
-    type: {
-      type: DataTypes.ENUM,
-      values: ['buy', 'sell'],
-      allowNull: false,
-    },
-    user_id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    symbol: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
-    shares: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        min: 1,
-        max: 100,
-      },
-    },
-    price: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    timestamp: {
-      type: DataTypes.BIGINT,
-      allowNull: false,
-    },
-  }, {
-    timestamps: false,
-  });
+  },
+  price: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  timestamp: {
+    type: DataTypes.BIGINT,
+    allowNull: false,
+  },
+}, {
+  timestamps: false,
+});
   
-  module.exports = Trade;
+module.exports = Trade;
